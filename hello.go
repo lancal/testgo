6eff46397401987c102143bf51b8f1cab0e8d5f6
Alan <ivda.calderon@gmail.com>
2017-08-18 15:27:43 -0300

    many main

--- hello.go
+++ hello.go
@@ -7,8 +7,28 @@ import "fmt"
 }*/
 
 
-func main(){
+/*func main(){
 
 	fmt.Println("1 + 1 = ", 1 + 1)
 
+}*/
+
+/*func main() {
+    fmt.Println(len("Hello World"))
+    fmt.Println("Hello World"[1])
+    fmt.Println("Hello " + "World")
+}*/
+
+/*func main() {
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}*/
+
+
+func main() {
+	var x string = "Hello World"
+	fmt.Println(x)
 }
