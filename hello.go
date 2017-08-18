f71ef04fd77423b7f0a0a8fef3cfcf84d1ba2770
Alan <ivda.calderon@gmail.com>
2017-08-18 17:13:59 -0300

    many main xD

--- hello.go
+++ hello.go
@@ -27,8 +27,82 @@ import "fmt"
 	fmt.Println(!true)
 }*/
 
-
+/*
 func main() {
 	var x string = "Hello World"
 	fmt.Println(x)
+}*/
+
+/*func main() {
+	var x string
+	x = "first"
+	fmt.Println(x)
+	x = "second"
+	fmt.Println(x)
+}*/
+
+/*unc	main(){
+
+	var x string
+	x = "first "
+	fmt.Println(x)
+	x = x + "second"
+	fmt.Println(x)
+}*/
+
+/*func main ()  {
+
+	var x string = "hello"
+	var y string = "world"
+	fmt.Println(x == y)
+
+}*/
+
+/*func main ()  {
+
+	x := "Hello World"
+	println(x)
+
+}*/
+
+/*func main ()  {
+
+	var x = "Hello World"
+	fmt.Println(x)
+
+}*/
+
+/*var ( a = 5 ; b = 10 ; c = 5 )
+
+var(
+
+	x1 = 12
+
+	y = 14
+
+	z = 15
+
+)
+
+*/
+
+/*func main() {
+	const x = "Hello World as constant"
+	fmt.Println(x)
+
+	fmt.Println(a , b , c)
+
+	fmt.Println(x1, y , z)
+}*/
+
+func main() {
+
+	var input float64
+
+	fmt.Print("Enter a number: ")
+	fmt.Scanf("%f", &input)
+	output := input * 2
+	fmt.Println(output)
 }
+
+
